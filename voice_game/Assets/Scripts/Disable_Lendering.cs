e5af90e7477984c5eda791c50e7806867d44b87c
bogyeongkim <qwt1501@khu.ac.kr>
2024-05-01 04:20:51 +0900

    stage1 1단계
    첫 번째 질문&마이크 입력 구현

---
+++ voice_game/Assets/Scripts/Disable_Lendering.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRendering : MonoBehaviour
+{
+    void Start()
+    {
+        // 이 스크립트가 붙어있는 게임 오브젝트의 렌더러 컴포넌트를 찾아서
+        Renderer renderer = GetComponent<Renderer>();
+
+        // 렌더러를 비활성화합니다.
+        if (renderer != null)
+        {
+            renderer.enabled = false;
+        }
+    }
+}
